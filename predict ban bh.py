247cd7a9bea569c3d6c5b3a9e3c912ba1e9ab789
huyenbui117 <62584585+huyenbui117@users.noreply.github.com>
2021-08-30 17:49:09 +0700

    ko tim  post nao

--- predict ban bh.py
+++ predict ban bh.py
@@ -6,41 +6,39 @@ es = Elasticsearch('http://103.74.122.196:9200')
 
 time_sleep = 1
 keywords = ['thương mại',
- 'nhân thọ',
- 'sức khỏe',
- 'tai nạn',
- 'y tế',
- 'phi nhân thọ',
- 'tài sản',
- 'thiệt hại',
- 'hàng hóa',
- 'hàng không',
- 'xe cơ giới',
- 'cháy nổ',
- 'tàu',
- 'trách nhiệm',
- 'tín dụng',
- 'tài chính',
- 'kinh doanh',
- 'nông nghiệp',
- 'tiền gửi',
- 'xã hội']
+            'nhân thọ',
+            'sức khỏe',
+            'tai nạn',
+            'y tế',
+            'phi nhân thọ',
+            'tài sản',
+            'thiệt hại',
+            'hàng hóa',
+            'hàng không',
+            'xe cơ giới',
+            'cháy nổ',
+            'tàu',
+            'trách nhiệm',
+            'tín dụng',
+            'tài chính',
+            'kinh doanh',
+            'nông nghiệp',
+            'tiền gửi',
+            'xã hội']
 
-
-
-
-result = []
+totalAmount = []
 for word in keywords:
-    list_userId = set()
+    list_message = set()
+    list_description = set()
     body = {
-        'size': 10000,
+        'size': 00,
         'query': {
             'bool': {
                 'must': [
                     {'bool': {
                         'should': [
                             {'match_phrase': {'description': word}},
-                            {'match_phrase': {'message':word}}
+                            {'match_phrase': {'message': word}}
                         ]
                     }},
                     {'bool': {
@@ -66,58 +64,61 @@ for word in keywords:
         # ]
     }
 
-    for month in range(7, 9):
-        if month == 7:
-            month = f'{month:02d}'
-            for i in range(1, 32):
-                day = i
-                day = f'{day:02d}'
-                index = f'dsminer_post_2021-{month}-{day}'
-                checkExcept = True
-                while checkExcept == True:
-                    try:
-                        response = es.search(index=index, body=body, request_timeout=30)['hits']['hits']
-                        for res in response:
-                            userId = res['_source']['ownerRef']['id']
-                            list_userId.add(userId)
+    month = 8
+    month = f'{month:02d}'
+    for i in range(27, 30):
+        day = i
+        day = f'{day:02d}'
+        index = f'dsminer_post_2021-{month}-{day}'
+        checkExcept = True
+        while checkExcept == True:
+            try:
+                response = es.search(index=index, body=body, request_timeout=30)['hits']['hits']
+                for res in response:
+                    message = res['_source']['message']
+                    description = res['_source']['description']
+                    list_message.add(message)
+                    list_description.add(description)
 
-                        print('pass' + ' ' + index)
-                        print(len(list_userId))
-                        time.sleep(1)
-                        checkExcept = False
-                    except:
-                        print(sys.exc_info()[0])
-                        print('errol' + ' ' + index)
-                        time.sleep(time_sleep)
-        else:
-            month = f'{month:02d}'
-            for i in range(1, 5):
-                day = i
-                day = f'{day:02d}'
-                index = f'dsminer_post_2021-{month}-{day}'
-                checkExcept = True
-                while checkExcept == True:
-                    try:
-                        response = es.search(index=index, body=body, request_timeout=30)['hits']['hits']
-                        for res in response:
-                            userId = res['_source']['ownerRef']['id']
-                            list_userId.add(userId)
+                print('pass' + ' ' + index)
+                print(len(list_message))
+                time.sleep(1)
+                checkExcept = False
+            except:
+                print(sys.exc_info()[0])
+                print('errol' + ' ' + index)
+                time.sleep(time_sleep)
+    # else:
+    #     month = f'{month:02d}'
+    #     for i in range(1, 5):
+    #         day = i
+    #         day = f'{day:02d}'
+    #         index = f'dsminer_post_2021-{month}-{day}'
+    #         checkExcept = True
+    #         while checkExcept == True:
+    #             try:
+    #                 response = es.search(index=index, body=body, request_timeout=30)['hits']['hits']
+    #                 for res in response:
+    #                     userId = res['_source']['ownerRef']['id']
+    #                     list_userId.add(userId)
+    #
+    #                 print('pass' + ' ' + index)
+    #                 print(len(list_userId))
+    #                 time.sleep(1)
+    #                 checkExcept = False
+    #             except:
+    #                 print(sys.exc_info()[0])
+    #                 print('errol' + ' ' + index)
+    #                 time.sleep(time_sleep)
 
-                        print('pass' + ' ' + index)
-                        print(len(list_userId))
-                        time.sleep(1)
-                        checkExcept = False
-                    except:
-                        print(sys.exc_info()[0])
-                        print('errol' + ' ' + index)
-                        time.sleep(time_sleep)
-    result.append(len(list_userId))
     path = r"post_amount.txt"
     out_file = open(path, "w", encoding="utf-8")
-    for i in result:
-        out_file.write(str(i) + '\n')
+    totalAmount = len(list_message)
+    for i in range(0, totalAmount):
+        out_file.write(str(list_message[i]) + '\n------------------\n'
+                       + str(list_description[i]) + '\n------------------------------------\n'
+                       )
     out_file.close()
 
-for i in result:
+for i in range(0, totalAmount):
     print(i)
-
