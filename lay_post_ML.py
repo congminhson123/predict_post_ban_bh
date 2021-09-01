ee36d1ac1cda9c5fa28c47d5e9323b28e9e8f59c
bim <46000077+congminhson123@users.noreply.github.com>
2021-09-02 03:19:33 +0700

    dang lam predict cong ty bao hiem cua user

---
+++ lay_post_ML.py
@@ -0,0 +1,110 @@
+import time
+import sys
+from elasticsearch import Elasticsearch
+from pandas import DataFrame
+
+es = Elasticsearch('http://103.74.122.196:9200')
+
+time_sleep = 1
+keywords = ['thương mại',
+            'nhân thọ',
+            'sức khỏe',
+            'tai nạn',
+            'y tế',
+            'phi nhân thọ',
+            'tài sản',
+            'thiệt hại',
+            'hàng hóa',
+            'hàng không',
+            'xe cơ giới',
+            'cháy nổ',
+            'tàu',
+            'trách nhiệm',
+            'tín dụng',
+            'tài chính',
+            'kinh doanh',
+            'nông nghiệp',
+            'tiền gửi',
+            'xã hội']
+list_post = list()
+docId_post = list()
+for word in keywords:
+    body = {
+        'size': 200,
+        'query': {
+            'bool': {
+                'must': [
+                    # {'bool': {
+                    #     'should': [
+                    #         {'match_phrase': {'description': word}},
+                    #         {'match_phrase': {'message': word}}
+                    #     ]
+                    # }},
+                    {'bool': {
+                        'should': [
+                            {'match_phrase': {'description': 'bảo hiểm'}},
+                            {'match_phrase': {'message': 'bảo hiểm'}}
+                        ]
+                    }},
+                    # {'match_phrase': {
+                    #     'docType': 'user_post'
+                    # }}
+                ],
+                # 'must_not': {
+                #     'match_phrase': {
+                #         'docType': 'page_post'
+                #     }
+                # }
+            }
+        },
+        'track_total_hits': True,
+        # '_source': [
+        #
+        # ]
+    }
+
+month = 8
+month = f'{month:02d}'
+for i in range(25, 29):
+    day = i
+    day = f'{day:02d}'
+    index = f'dsminer_post_2021-{month}-{day}'
+    checkExcept = True
+    while checkExcept:
+        try:
+            response = es.search(index=index, body=body, request_timeout=30)['hits']['hits']
+            for res in response:
+                message = res['_source']['message']
+                description = res['_source']['description']
+                docId = res['_source']['docId']
+                if message is not None:
+                    list_post.append(message)
+                    docId_post.append(docId)
+                if description is not None:
+                    list_post.append(description)
+                    docId_post.append(docId)
+
+            # print('pass' + ' ' + index)
+            # print(len(list_message))
+            time.sleep(1)
+            checkExcept = False
+        except:
+            print(sys.exc_info()[0])
+            print('errol' + ' ' + index)
+            time.sleep(time_sleep)
+# path = r"post_amount.txt"
+# out_file = open(path, "w", encoding="utf-8")
+# list_data = []
+# messages = list(list_message)
+# descriptions = list(list_description)
+# totalAmount = len(descriptions)
+# for i in range(0, totalAmount):
+#     data = ('MESSAGE\n' + str(messages[i])
+#             +  '\nDESCRIPTION\n' + str(descriptions[i])
+#             )
+#     list_data.append(data)
+#     print(i)
+
+df = DataFrame({'docId_post':docId_post, 'post':list_post})
+df.to_excel(r'data.xlsx', encoding='utf-8')
+
